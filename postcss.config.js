6075ed4840f41f49eab551ca67068061f73ec0cd
user <pengfluf@tutanota.com>
2024-10-26 20:55:24 +0300

    oklch palette

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
+import path from 'node:path';
+
+import mixins from 'postcss-mixins';
+import simpleVars from 'postcss-simple-vars';
 import nested from 'postcss-nested';
 import presetEnv from 'postcss-preset-env';
 
 export default {
-  plugins: [nested(), presetEnv()],
+  plugins: [
+    mixins({ mixinsDir: path.resolve('./src/styles/mixins') }),
+    simpleVars(),
+    nested(),
+    presetEnv(),
+  ],
 };
